fdf2aa9f7e89db16c90a7a7b75da35a11fb39013
Lucas-EstudanteIFC <alunolucasmartins62@gmail.com>
2024-07-12 15:14:57 -0300

    criação de componentes

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ExpandBox from './components/ExpandBox.vue';
+import DarkTheme from './components/DarkTheme.vue';
+</script>
+
+<template>
+  <h1>Compenentes</h1>
+  <hr>
+  <ExpandBox title="Caixa-1" content="Conteúdo da caixa 1" />
+  <ExpandBox title="Caixa-2" content="Conteúdo da caixa 2" />
+  <DarkTheme />
+</template>
+
+<style scoped></style>
